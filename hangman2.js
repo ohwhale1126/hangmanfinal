98a72e42934525a93f9f8ea63d3a681c86c7e686
Gemma Longinotti <Gem@Gemmas-MacBook-Pro.local>
2017-05-09 10:26:08 -0700

    trying to fix error with guessing

--- hangman2.js
+++ hangman2.js
@@ -14,29 +14,30 @@ var terms = [
 ]
 var random = Math.floor((Math.random() * (terms.length - 1)));
 var wordChooser = terms[random]; // the word to guess will be chosen from the array above
-var newWord = new Array(wordChooserhooser.length); //ratewort= approx "evaluate word"
+var newWord = new Array(wordChooser.length); //ratewort= approx "evaluate word"
 var error = 0; //fehler = error
+console.log(wordChooser);
 // every letter in the word is symbolized by an underscore in the guessfield
-for (var i = 0; i < newWordord.length; i++) {
+for (var i = 0; i < newWord.length; i++) {
     newWord[i] = "_ ";
 }
 // prints the guessfield
 function showNewWord() {
     for (var i = 0; i < newWord.length; i++) {
-        var guessfield = document.getElementById("guessfield");
-        var letter = document.createTextNode(newword[i]); //buchstabe = letter
-        guessfield.appendChild(letter); //ratefeld = guess field
+        var guessField = document.getElementById("guessfield");
+        var letter = document.createTextNode(newWord[i]); //buchstabe = letter
+        guessField.appendChild(letter); //ratefeld = guess field
     }
 }
 //checks if the the letter provided by the user matches one or more of the letters in the word
 function checkCharacter() {
     var a = document.blank; //check .rateformular - what's it's function
     var b = a.elements["newcharacter"];
-    var character = b.value; // the letter provided by the user ZEICHEN = character
+    var character = b.value; // the letter provided by the user
     character = character.toUpperCase();
     for (var i = 0; i < wordChooser.length; i++) {
-        if (wordchooser[i] === character) {
-            newword[i] = character + " ";
+        if (wordChooser[i] === character) {
+            newWord[i] = character + " ";
             var goal = true;
         }
         character = "";
@@ -44,7 +45,7 @@ function checkCharacter() {
     //deletes the guessfield and replaces it with the new one
     var guessField = document.getElementById("guessfield");
     guessField.innerHTML = "";
-    shownewword();
+    showNewWord();
     // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
     if (!goal) {
         var guessedLetter = document.getElementById("guessedletter");
@@ -66,7 +67,7 @@ function checkCharacter() {
     }
     //once you got six wrong letters, you lose
     if (error === 6) {
-        window.alert("Uh...I guess you're dead now.");
+        window.alert("You lose :( .");
     }
 }
 
